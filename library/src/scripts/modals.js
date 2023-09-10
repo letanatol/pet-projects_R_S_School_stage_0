ca2fcc2f37bcc3541544a50b6bc7fb57ec5d15ce
letanatol <letanatol@gmail.com>
2023-09-11 00:03:08 +0200

    fix: change authorization of the user

--- library/src/scripts/modals.js
+++ library/src/scripts/modals.js
@@ -1,22 +1,11 @@
-function openModal(elem) {
+export function openModal(elem) {
   elem.classList.replace('modal_state_close', 'modal_state_open');
   body.classList.add('_scroll');
   blackout.classList.add('_blackout__open');
 }
 
-function closeModal(elem) {
+export function closeModal(elem) {
   elem.classList.replace('modal_state_open', 'modal_state_close');
   body.classList.remove('_scroll');
   blackout.classList.remove('_blackout__open');
 }
-
-
-
-
-
-
-export {
-  openModal,
-  closeModal
-}
-
--- library/src/scripts/registration.js
+++ library/src/scripts/registration.js
@@ -17,21 +17,24 @@ const modalProfileInitials = document.querySelector('.modal-profile__initials');
 const modalProfileSurname = document.querySelector('.modal-profile__surname');
 const infoVisits = document.getElementById('infoVisits');
 const infoBooks = document.getElementById('infoBooks');
+const modalProfileCardNumber = document.getElementById('modal-profile-card-number');
 const closeBtnModalProfile = document.getElementById('modal-profile__button-close');
 const dropMenuButtonLogout = document.querySelector('.drop-menu__button-logout');
 const findTitle = document.querySelector('.find__title');
 const findFormInputName = document.getElementById('find__form-input-name');
 const findFormInputNumber = document.getElementById('find__form-input-number');
 const findFormButton = document.querySelector('.find__form-button');
+
 const libraryCardsInfo = document.querySelector('.library-cards__info');
 const libraryCardsGet = document.querySelector('.library-cards__get');
 const libraryCardsVisit = document.querySelector('.library-cards__visit');
+const infoVisitsCard = document.getElementById('infoVisitsCard');
+const infoBooksCard = document.getElementById('infoBooksCard');
+const libraryCardsButtonProfile = document.getElementById('get-button-profile');
 
 
 formRegister.addEventListener('submit', submit);
 
-
-//регистрация пользователя с сохранением в storage и одновременным log in 
 function submit(event) {
   event.preventDefault();
   const formData = new FormData(formRegister);
@@ -44,18 +47,146 @@ function submit(event) {
   const cardNumber = getCardNumber();
   formData.set('cardNumber', cardNumber);
 
-  const userData = {};
+  const userCurrentData = {};
   for (let item of formData.entries()) {
     let key = item[0];
     let value = item[1];
-    userData[key] = value;
+    userCurrentData[key] = value;
   }
-  localStorage.setItem('currentUser', JSON.stringify(userData));
-  renderAuthorizedUI();
+
+  saveUsersData('userCurrentData', userCurrentData); // использование данных для renderAuthorizedUI
+
+  const users = getUsersData('users');
+  if (users) {
+    saveUsersData('users', [...users, userCurrentData]);
+  } else {
+    saveUsersData('users', [userCurrentData]);
+  }
+
   closeModal(modalRegister);
+  renderAuthorizedUI();
 }
 
-function getCardNumber() {
+function renderAuthorizedUI() {
+  const userCurrentData = getUsersData('userCurrentData');
+  const { firstName, lastName, cardNumber, visits, booksCount } = userCurrentData;
+
+  // 1 меняем иконку на инициалы
+  createIconInitials(firstName, lastName);
+
+  // 2 меняем модалку дроп-меню
+  dropMenuNoAuthorization.style.display = 'none';
+  dropMenuAuthorization.style.display = 'block';
+  dropMenuTitle.textContent = cardNumber;
+
+  // 3 открыть модалку  buy card в books:
+  bookBtn.forEach(button => {
+    button.addEventListener('click', () => {
+      openModal(modalBuy);
+    })
+  })
+
+  //3 закрыть модалку  buy card в books:
+  blackout.addEventListener('click', () => {
+    closeModal(modalBuy);
+  })
+
+  //3 закрыть модалку buy card на крестик:
+  closeBtnModalBuy.addEventListener('click', () => {
+    closeModal(modalBuy);
+  })
+
+  //4 открыть модалку profile
+  dropMenuButtonProfile.addEventListener('click', () => {
+    openModal(modalProfile);
+  })
+
+  //4 открыть модалку profile в libraryCard
+  libraryCardsButtonProfile.addEventListener('click', () => {
+    openModal(modalProfile);
+  })
+
+  createIconInitialsForProfile(firstName, lastName);
+  infoVisits.innerHTML = visits;
+  infoBooks.innerHTML = booksCount;
+  modalProfileCardNumber.innerHTML = cardNumber;
+
+  //4 закрыть модалку profile на крестик:
+  closeBtnModalProfile.addEventListener('click', () => {
+    closeModal(modalProfile);
+  })
+
+  //4 закрыть модалку profile:
+  blackout.addEventListener('click', () => {
+    closeModal(modalProfile);
+  })
+
+  // 5 digital card
+  findTitle.textContent = 'Your Library card';
+  findFormInputName.value = firstName;
+  findFormInputNumber.value = cardNumber;
+  infoVisitsCard.innerHTML = visits;
+  infoBooksCard.innerHTML = booksCount;
+  findFormButton.style.display = 'none';
+  libraryCardsInfo.style.display = 'flex';
+  libraryCardsGet.style.display = 'none';
+  libraryCardsVisit.style.display = 'block';
+
+  // log Out
+  // dropMenuButtonLogout.addEventListener('click', () => {
+  //   let userLogOut = getUserData(); //todo delete!!!!!!!!!!!!!!!!!!!!!!!!!
+  //   userLogOut.isAuthorized = false;
+  //   localStorage.setItem('currentUser', JSON.stringify(userLogOut));
+  //   renderUnknownUI();
+  // });
+
+}
+
+// function renderUnknownUI() {
+//   // 1 меняем инициалы на иконку
+//   userIcon.innerHTML = '';
+//   userIcon.classList.remove('header__icon-authorization');
+//   userIcon.removeAttribute("title");
+
+//   // 2 меняем модалку дроп-меню
+//   dropMenuNoAuthorization.style.display = 'block';
+//   dropMenuAuthorization.style.display = 'none';
+//   dropMenuTitle.textContent = '';
+
+//   // 3 открыть modalLogin в books:
+//   bookBtn.forEach(button => {
+//     button.addEventListener('click', () => {
+//       openModal(modalLogin);
+//     })
+//   })
+//   // 4 закрыть модалку modalLogin в books:
+//   blackout.addEventListener('click', () => {
+//     closeModal(modalBuy);
+//   })
+//   // закрыть модалку modalLogin на крестик:
+//   closeBtnModalBuy.addEventListener('click', () => {
+//     closeModal(modalBuy);
+//   })
+//   // digital card
+//   findTitle.textContent = 'Find your Library card';
+//   findFormInputName.value = '';
+//   findFormInputNumber.value = '';
+//   findFormButton.style.display = 'block';
+//   libraryCardsInfo.style.display = 'none';
+//   libraryCardsGet.style.display = 'block';
+//   libraryCardsVisit.style.display = 'none';
+// }
+
+
+export function saveUsersData(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getUsersData(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export function getCardNumber() {
   let min = Math.ceil(100_000_000);
   let max = Math.floor(999_999_999);
   let cardNumber = (Math.floor(Math.random() * (max - min + 1)) + min).toString('16').toUpperCase();
@@ -65,186 +196,25 @@ function getCardNumber() {
   return cardNumber;
 }
 
-export function getUserData() {
-  return JSON.parse(localStorage.getItem('currentUser'));
-}
-
-let userAuthorized = getUserData();
-
-function isUserAuthorized() {
-  return getUserData()?.isAuthorized;
-}
-
-function updatePage() {
-  if (isUserAuthorized) {
-    renderAuthorizedUI();
-  } else {
-    renderUnknownUI();
-  }
-}
-
-// updatePage();
-
-function renderAuthorizedUI() {
-  // 1 меняем иконку на инициалы
-  createIconInitials(userAuthorized.firstName, userAuthorized.lastName);
-  // 2 меняем модалку дроп-меню
-  dropMenuNoAuthorization.style.display = 'none';
-  dropMenuAuthorization.style.display = 'block';
-  dropMenuTitle.textContent = userAuthorized.cardNumber;
-  // 3 открыть модалку buy card в books:
-  bookBtn.forEach(button => {
-    button.addEventListener('click', () => {
-      openModal(modalLogin);
-    })
-  })
-  // закрыть модалку buy card в books:
-  blackout.addEventListener('click', () => {
-    closeModal(modalLogin);
-  })
-  // закрыть модалку buy card на крестик:
-  modalLogin.addEventListener('click', () => {
-    closeModal(modalLogin);
-  })
-
-  //5 открыть модалку profile
-  dropMenuButtonProfile.addEventListener('click', () => {
-    openModal(modalProfile);
-  })
-  createIconInitialsForProfile(userAuthorized.firstName, userAuthorized.lastName);
-  infoVisits.innerHTML = userAuthorized.visits;
-  infoBooks.innerHTML = userAuthorized.booksCount;
-  // закрыть модалку profile на крестик:
-  closeBtnModalProfile.addEventListener('click', () => {
-    closeModal(modalProfile);
-  })
-  // закрыть модалку profile:
-  closeBtnModalProfile.addEventListener('click', () => {
-    closeModal(modalProfile);
-  })
-  // 6 digital card
-  findTitle.textContent = 'Your Library card';
-  findFormInputName.value = userAuthorized.firstName;
-  findFormInputNumber.value = userAuthorized.cardNumber;
-  findFormButton.style.display = 'none';
-  libraryCardsInfo.style.display = 'flex';
-  libraryCardsGet.style.display = 'none';
-  libraryCardsVisit.style.display = 'block';
-
-
-
-  // log Out
-  dropMenuButtonLogout.addEventListener('click', () => {
-    let userLogOut = getUserData();
-    userLogOut.isAuthorized = false;
-    localStorage.setItem('currentUser', JSON.stringify(userLogOut));
-    renderUnknownUI();
-  });
-
-}
-function createIconInitials(firstName, lastName) {
+export function createIconInitials(firstName, lastName) {
   userIcon.innerHTML = firstName.slice(0, 1).toUpperCase() + lastName.slice(0, 1).toUpperCase();
   userIcon.classList.add('header__icon-authorization');
   userIcon.title = `${firstName} ${lastName}`;
+  console.log('я выполнилась');
 }
 
-function createIconInitialsForProfile(firstName, lastName) {
+export function createIconInitialsForProfile(firstName, lastName) {
   modalProfileInitials.innerHTML = firstName.slice(0, 1).toUpperCase() + lastName.slice(0, 1).toUpperCase();
   modalProfileSurname.innerHTML = `${firstName} ${lastName}`;
 }
 
-
-
-function renderUnknownUI() {
-  // 1 меняем инициалы на иконку
-  userIcon.innerHTML = '';
-  userIcon.classList.remove('header__icon-authorization');
-  userIcon.removeAttribute("title");
-
-  // 2 меняем модалку дроп-меню
-  dropMenuNoAuthorization.style.display = 'block';
-  dropMenuAuthorization.style.display = 'none';
-  dropMenuTitle.textContent = '';
-
-  // 3 открыть modalLogin в books:
-  bookBtn.forEach(button => {
-    button.addEventListener('click', () => {
-      openModal(modalLogin);
-    })
-  })
-  // 4 закрыть модалку modalLogin в books:
-  blackout.addEventListener('click', () => {
-    closeModal(modalBuy);
-  })
-  // закрыть модалку modalLogin на крестик:
-  closeBtnModalBuy.addEventListener('click', () => {
-    closeModal(modalBuy);
-  })
-  // digital card
-  findTitle.textContent = 'Find your Library card';
-  findFormInputName.value = '';
-  findFormInputNumber.value = '';
-  findFormButton.style.display = 'block';
-  libraryCardsInfo.style.display = 'none';
-  libraryCardsGet.style.display = 'block';
-  libraryCardsVisit.style.display = 'none';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// formRegister.addEventListener('submit', event => {
-//   event.preventDefault();
-
-//   const firstName = event.currentTarget.firstName.value;
-//   const lastName = event.currentTarget.lastName.value;
-//   const email = event.currentTarget.email.value;
-//   const password = event.currentTarget.password.value;
-//   // const cardNumber = getCardNumber();
-//   const userData = {
-//     firstName,
-//     lastName,
-//     email,
-//     password,
-//     // cardNumber,
-//     count: 1,
-//     isAuthorized: true,
+//! window onload
+// function updatePage() {
+//   if (isUserAuthorized) {
+//     renderAuthorizedUI();
+//   } else {
+//     renderUnknownUI();
 //   }
-
-//   localStorage.setItem('currentUser', JSON.stringify(userData));
-//   renderAuthorizedUI();
-//   closeModal(modalRegister);
-// })
-
-// if (!localStorage.getItem('users')) {
-//   localStorage.setItem('users', JSON.stringify(userData));
-// } else {
-//   const users = JSON.parse(localStorage.getItem("users"));
-//   console.log(users);
-//   const arrayUsers = [users, userData];
-//   localStorage.setItem('users', JSON.stringify(arrayUsers));
 // }
 
-// }
-
-// formRegister.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   getUserData(formRegister);
-//   console.log(formRegister);
-// })
-
-
-
+// updatePage();
