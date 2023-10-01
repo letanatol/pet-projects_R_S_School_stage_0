6f81145febd1d449ee58eb6ef513e9bbed01e7ee
letanatol <letanatol@gmail.com>
2023-10-01 23:52:18 +0200

    feat: add function getPhotosQuery

--- image-galery/src/galery-cards/index.js
+++ image-galery/src/galery-cards/index.js
@@ -3,6 +3,9 @@ const iconCross = document.getElementById('icon-cross');
 const iconSearch = document.getElementById('icon-search');
 const mainPhotos = document.getElementById('main-photos');
 
+window.addEventListener('load', getPhotos());
+iconSearch.addEventListener('click', getPhotosQuery);
+iconCross.addEventListener('click', closeSearch);
 
 async function getPhotos(query = 'sunset') {
   const url = `https://api.unsplash.com/search/photos?client_id=eQ31g4OVoE2n40OnuWIHfBZ6yw2tHPnJ85C-QC6waoQ&per_page=9&orientation=landscape&query=${query}`;
@@ -16,6 +19,23 @@ async function getPhotos(query = 'sunset') {
   }
 }
 
+async function getPhotosQuery() {
+  if (inputSearch.value !== "") {
+    iconSearch.classList.replace('state_open', 'state_close');
+    iconCross.classList.replace('state_close', 'state_open');
+    const url = `https://api.unsplash.com/search/photos?client_id=eQ31g4OVoE2n40OnuWIHfBZ6yw2tHPnJ85C-QC6waoQ&per_page=9&orientation=landscape&query=${inputSearch.value}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const photos = data.results;
+      removePhotos();
+      showPhotos(photos);
+    } catch (error) {
+      console.log(error);
+    }
+  }  
+}
+
 function showPhotos(photos) {
   photos.map((item) => {
     const div = document.createElement('div');
@@ -25,4 +45,12 @@ function showPhotos(photos) {
   });
 }
 
-window.addEventListener('load', getPhotos());
+function removePhotos() {
+  mainPhotos.replaceChildren();
+}
+
+function closeSearch() {
+  iconCross.classList.replace('state_open', 'state_close');
+  iconSearch.classList.replace('state_close', 'state_open');
+  inputSearch.value = '';
+}
